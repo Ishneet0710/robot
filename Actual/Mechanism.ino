83240ad5abe101ff46856a808014d81b161a8032
ishneet <ishneetsingh0710@gmail.com>
2021-12-30 14:58:31 +0800

    update files

--- Actual/Mechanism.ino
+++ Actual/Mechanism.ino
@@ -1,8 +1,8 @@
 
 #include <Servo.h>
 
-const int trigpin = 9;
-const int echopin = 10;
+const int trigpin = 11;
+const int echopin = 12;
 long duration;
 int distance;
 
@@ -20,23 +20,15 @@ void setup() {
   scoop.write(0);
   delay(3000);
 
-  if(distance > 10 && distance < 15){
-    for (int i = 0; i < 11; ++i){
-      scoop.write(i*10);
-      delay(75);
-    }
-    
-      //  scoop.write(80);
-      //  delay(1000);
-      //  scoop.write(120);
-    
-    delay(3000);
-    backdoor.write(90);
-  }
   
 }
 
 void loop() {
+
+  bool automated = 1; // change to 0 after 30 seconds (upon a press of a button?)
+
+
+  
   digitalWrite(trigpin, LOW);
   delayMicroseconds(2);
 
@@ -44,7 +36,30 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigpin, LOW);
 
-  duration = pulseIn(echopin, HIGH);
-  distance = duration * 0.034 / 2;
+  
 
+  //automated period
+  while (automated){
+
+     // if (button_pressed) {automated = 0; break;}
+    duration = pulseIn(echopin, HIGH);
+    distance = duration * 0.034 / 2;
+  
+    
+    Serial.print("Distance: ");
+    Serial.println(distance);
+  
+    if(distance >= 10 and distance <= 15){
+      for (int i = 0; i < 11; ++i){ //scoop up ball
+        scoop.write(i*10);
+        delay(75);
+      }
+      delay(1500);
+      scoop.write(0); // wait 1.5 second, then scoop back down
+      continue; // back to start of while loop
+    }
+  }
+
+  //manual period
+  
 }
--- Actual/Scoop.ino
+++ Actual/Scoop.ino
@@ -4,7 +4,7 @@
 Servo myservo;  
 
 void setup() {
-  myservo.attach(9);
+  myservo.attach(8);
   myservo.write(0);
   delay(2000);
   myservo.write(120);
