227e1b0d796fb893370a3e172a99120bee39b69a
Ishneet0710 <ishneetsingh0710@gmail.com>
2022-01-03 22:09:41 +0800

    update

--- Actual/Mechanism/Mechanism.ino
+++ Actual/Mechanism/Mechanism.ino
@@ -1,10 +1,21 @@
-
 #include <Servo.h>
 
-const int trigpin = 11;
-const int echopin = 12;
+//const int trigpin = 11;
+//const int echopin = 12;
 long duration;
 int distance;
+
+int motor1pin1 = 2;
+int motor1pin2 = 3;
+
+int motor2pin1 = 4;
+int motor2pin2 = 5;
+
+int motor3pin1 = 6;
+int motor3pin2 = 7;
+
+int motor4pin1 = 8;
+int motor4pin2 = 9;
 
 
 Servo scoop;  
@@ -37,30 +48,69 @@ void reset_backdoor(int gahma){
   return;
 }
 
+void forward(){
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
 
-//void cock{bool balls}{ // balls is 0 if turn left & 1 if turn right
-//  if (balls) {
-//    turn right
-//  }
-//  else {
-//    turn left
-//  }
-//}
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+  //delay(1000);
+
+  digitalWrite(motor3pin1, HIGH);
+  digitalWrite(motor3pin2, LOW);
+
+  digitalWrite(motor4pin1, HIGH);
+  digitalWrite(motor4pin2, LOW);
+  //delay(dulta);
+}
+
+void please_stop_i_want_to_die(){
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, LOW);
+  //delay(1000);
+
+  digitalWrite(motor3pin1, LOW);
+  digitalWrite(motor3pin2, LOW);
+
+  digitalWrite(motor4pin1, LOW);
+  digitalWrite(motor4pin2, LOW);
+}
+
 
 void setup() {
-  pinMode(trigpin, OUTPUT);
-  pinMode(echopin, INPUT);
+//  pinMode(trigpin, OUTPUT);
+//  pinMode(echopin, INPUT);
   Serial.begin(9600);
-  backdoor.attach(9);
+  backdoor.attach(12);
   backdoor.write(180);
-  scoop.attach(8);
+  scoop.attach(11);
   scoop.write(0);
-  delay(3000);
-    for (int i = 0; i < 2; ++i){
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+  pinMode(motor3pin1, OUTPUT);
+  pinMode(motor3pin2, OUTPUT);
+  pinMode(motor4pin1, OUTPUT);
+  pinMode(motor4pin2, OUTPUT);
+  //forward(1000);
+  delay(2000);
+  for (int i = 0; i < 2; ++i){//change to 3 later
+    forward();
+    delay(2000);
+    please_stop_i_want_to_die();
+    delay(500);
     scoop_ball(2000);
-    reset_scoop(5000);}
+    reset_scoop(1500);
+    }
     trigger_backdoor(3000);
-    reset_backdoor(1000);
+    reset_backdoor(500);
+    forward();
+    delay(2000);
+    please_stop_i_want_to_die();
 }
 
 
@@ -96,5 +146,6 @@ void loop() {
 //  }
 
   //manual period
+  // what in the actual fuck
   
 }
